ca13ae92d31b9dd8fdf0a77f619cf0f8055b97e5
priscila miguel <priscilamiguel579@gmail.com>
2023-12-31 16:14:51 -0300

    CONLUIDO

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+
+            Nokia nokiaPhone = new Nokia("123456", "Nokia XYZ", "IMEI123", 64);
+            nokiaPhone.Ligar();
+            nokiaPhone.InstalarAplicativo("WhatsApp");
+            
+
+            Console.WriteLine("\n");
+
+            
+            Iphone iphone = new Iphone("789012", "iPhone 12", "IMEI456", 128);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
